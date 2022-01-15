d384e02461a552b5ce7669b354e283f25804a98e
g-rden <94605617+g-rden@users.noreply.github.com>
2022-01-15 18:21:22 +0100

    Update getframes.sh

--- getframes.sh
+++ getframes.sh
@@ -7,11 +7,12 @@ scene="$1"
 input="$2"
 img_nbr=0
 pts_time_prev=0
+dir='frames'
 
 #output first frame
-ffmpeg -i "$input" -vframes 1 img/img0.png
+ffmpeg -i "$input" -vframes 1 "$dir"/img0.png
 #output all other detected frames
-ffmpeg -i "$input" -filter_complex "select='gt(scene,$scene)',metadata='print:file=time.txt'" -vsync vfr img/img%00d.png
+ffmpeg -i "$input" -filter_complex "select='gt(scene,$scene)',metadata='print:file=time.txt'" -vsync vfr "$dir"/img%00d.png
 
 #clear file
 printf '' > concat.txt
@@ -22,7 +23,7 @@ while read -r line; do
 		*pts_time:*) pts_time="${line##*pts_time:}"
 			dur=$( (echo "$pts_time $pts_time_prev" | awk '{print $1-$2}'))
 			pts_time_prev=$pts_time
-			printf 'file img/img%d.png\nduration %s\n' "$img_nbr" "$dur" >> concat.txt
+			printf 'file %s/img%d.png\nduration %s\n' "$dir" "$img_nbr" "$dur" >> concat.txt
 			img_nbr=$((img_nbr+1))
 		;;
 	esac
@@ -32,6 +33,6 @@ done < time.txt
 pts_time=$( (ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "$input"))
 dur=$( (echo "$pts_time $pts_time_prev" | awk '{print $1-$2}'))
 pts_time_prev=$pts_time
-printf 'file img/img%d.png\nduration %s\nfile img/img%d.png\nduration 0\n' "$img_nbr" "$dur" "$img_nbr" >> concat.txt
+printf 'file %s/img%d.png\nduration %s\nfile %s/img%d.png\nduration 0\n' "$dir" "$img_nbr" "$dur" "$dir" "$img_nbr" >> concat.txt
 
 #you might need to edit concat.txt to fix duration etc.
